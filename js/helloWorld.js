fb35fbc9170c0a87bb2f3c295bdb23a12cd6ed8c
Chris Jones <chris.jones@spongeuk.com>
2013-10-25 14:26:34 +0100

    dummy plugin

---
+++ js/helloWorld.js
@@ -0,0 +1,21 @@
+define(["coreJS/adapt", "templates"], function(Adapt) {
+    
+    Adapt.register('helloWorld', 'view', {
+        
+        el: "#wrapper",
+        
+        initialize: function() {
+            this.render();
+        },
+        
+        render: function() {
+            var data = {"title":"Hello World2"};
+            var template = Handlebars.templates["helloWorld"];
+            this.$el.append(template(data));
+            return this;
+        }
+        
+    });
+    
+    
+})
